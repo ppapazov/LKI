8c601bebfc8de2b1f6f911fbb82f8a8a9ada2baf
pavelpapazov <4work.tasks@gmail.com>
2018-01-24 18:31:22 +0200

    Adding data driving testing based on CSV file

--- tests/courses/register_courses_csv_data.py
+++ tests/courses/register_courses_csv_data.py
@@ -4,22 +4,25 @@ from base.selenium_driver import SeleniumDriver
 import unittest
 import pytest
 from ddt import ddt, data, unpack
-import  time
+from utilities.read_file import getCSVdata
+import time
 
 
 @pytest.mark.usefixtures("oneTimeSetUp", "setUp")
 @ddt
-class RegisterCoursesTest(unittest.TestCase):
+class RegisterCoursesCSVDataTests(unittest.TestCase):
 
     @pytest.fixture(autouse=True)
-    def classSetup(self, oneTimeSetUp):
+    def objectSetup(self, oneTimeSetUp):
         self.courses = RegisterCoursesPage(self.driver)
         self.ts = TStatus(self.driver)
         self.d = SeleniumDriver(self.driver)
 
+    def setUp(self):
+        self.driver.get("https://letskodeit.teachable.com")
+
     @pytest.mark.run(order=1)
-    @data(("JavaScript for beginners", "5457 0822 3591 3352", "12 / 19", "123", "01234"),
-          ("Learn Python 3 from scratch", "5457 0822 3591 3352", "12 / 20", "123", "01234"))
+    @data(*getCSVdata("/Users/pavelpapazov/PycharmProjects/LetsKodeit/test_data.csv"))
     @unpack
     def test_invalidEnrollment(self, courseName, ccNum, ccDate, ccCvv, postal):
         self.courses.openCousesPage()
@@ -27,10 +30,11 @@ class RegisterCoursesTest(unittest.TestCase):
         self.courses.selectCoureToEnroll(courseName)
         self.courses.startEnroll()
         self.courses.enterEmail("s@yopmail.com")
+        time.sleep(1)
         self.courses.enrollCource(num=ccNum, exp=ccDate, cvv=ccCvv, postal=postal)
         result = self.courses.verifyEnrollFailed()
         self.ts.markFinal("test_invalidEnrollment", result, "Enrollment Failed verification")
-        self.driver.get("https://letskodeit.teachable.com")
+
 
 
 
